ddc46db3e825ac5385a57c2b5eaa559c4e9aeb70
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-07-11 15:48:33 +0100

    Add files via upload

---
+++ englandwin.js
@@ -0,0 +1,15 @@
+
+
+
+const englandwin = "YESSS COME ONE WE WIN, and WIN BIG!";
+const cwin = "oh no the dream is over";
+
+
+
+let dice = Math.random();
+	if (dice >= 0.5) {
+		alert(englandwin)
+	} else {
+		alert(cwin)
+	};
+
