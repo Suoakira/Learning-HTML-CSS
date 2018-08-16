6ddfe82adc0ca8e9d6c5c5e3ba6297b62bf83fc9
Stephen <37964234+Suoakira@users.noreply.github.com>
2018-08-16 16:04:14 +0100

    Add files via upload

--- basicJsTest.js
+++ basicJsTest.js
@@ -147,4 +147,12 @@ document.getElementById("demo").innerHTML = toCelsius;
    window.status = "<TYPE YOUR MESSAGE>"; 
 
    // continuation of the the eloquent theory of javascript
+
+   const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
  
